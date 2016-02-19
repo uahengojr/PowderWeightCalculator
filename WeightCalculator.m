3dca714886c81ddc4482b55306adbf7cce264cb6
uahengojr <uahengo@me.com>
2016-02-18 19:44:40 -0800

    Update caluclate button.

--- WeightCalculator.m
+++ WeightCalculator.m
@@ -199,7 +199,7 @@ function pushbutton1_Callback(hObject, ~, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-display(number_of_atoms);
+display('Calcualte Button Pressed');
 
 
 % --- Executes on button press in pushbutton2.
@@ -207,6 +207,6 @@ function pushbutton2_Callback(hObject, ~, handles)
 % hObject    handle to pushbutton2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-display('Resert Button Pressed');
+display('Reset Button Pressed');
 
 
