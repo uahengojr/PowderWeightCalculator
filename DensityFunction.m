d0954da47f6a8e151b48d75a1ff8d90b01de4980
uahengojr <uahengo@me.com>
2016-04-07 17:45:50 -0700

    "Read and convert input to doubles."

--- DensityFunction.m
+++ DensityFunction.m
@@ -2,6 +2,15 @@ function [ output_args ] = DensityFunction( input_args )
 %DENSITYFUNCTION Summary of this function goes here
 %   Detailed explanation goes here
 
+density = str2double(get(input_args.density, 'String'));
+diameter = str2double(get(input_args.diameter, 'String'));
+atom_number = str2double(get(input_args.atom_number, 'String'));
+thickness = str2double(get(input_args.thickness, 'String'));
+
+
+density = density * 100.00;
+
+output_args = density;
 
 end
 
